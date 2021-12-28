9c95857824f8878838fac94cc3347de4c7bbae0b
Jan Walther <44636333+JanEricWalther@users.noreply.github.com>
2021-12-28 13:21:18 +0100

    formatting

--- caesar_cipher.js
+++ caesar_cipher.js
@@ -5,33 +5,25 @@
  */
 function rot13(str) {
 
-  // Split String up into Char Array
   str.split('');
   let result = [];
 
   for(let i=0; i<str.length; i++) {
     let c = str.charCodeAt(i);
 
-    // check if Char at i is between 'A' and 'Z'
     if(c >= 'A'.charCodeAt(0) && c <= 'Z'.charCodeAt(0)) {
 
-      // check if Char at i is after 'N', so 13 gets subtracted
       if(c >= 'N'.charCodeAt(0)) {
         result.push(String.fromCharCode(c - 13));
-      }
-      else {  // Char at i is before 'N', so 13 gets added
+      } else {
         result.push(String.fromCharCode(c + 13))
       }
-    }
-    // Non 'A'-'Z' gets just ignored
-    else {
+    } else {
       result.push(str[i])
     }
   }
   
-  // join Char Array back together
   return result.join('');
 }
 
-// Change the inputs below to test
 console.log(rot13("SERR PBQR PNZC"));
